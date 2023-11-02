0136ef803ec978d96d3af2060709f23a92a8429d
Dylan McCall <dylan@endlessos.org>
2023-11-02 14:39:28 -0700

    Add single instance lock and improve app lifecycle
    To support this, code related to the application window has been moved
    to a separate module.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-const {
-  app, BrowserWindow, session, webContents, dialog
-} = require('electron');
+const { app } = require('electron');
 const prompt = require('electron-prompt');
 const path = require('path');
-const storage = require('electron-json-storage');
-const log = require('electron-log');
-const { isWhiteListed } = require('./utils/whiteListDomains');
-const { registerShortcuts } = require('./utils/shortcuts');
-let deepLink = null;
+const { showURL } = require('./window');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -44,109 +38,31 @@ if (!app.isDefaultProtocolClient('gsm')) {
   app.setAsDefaultProtocolClient('gsm');
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 900,
-    show: false,
-    webPreferences: {
-      plugins: true,
-      enableRemoteModule: true,
-      nodeIntegration: true,
-      webviewTag: true,
-    },
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory, additionalData) => {
+    const urlArg = commandLine[1];
+    showURL(urlArg);
   });
-
-  mainWindow.loadURL('https://gamestarmechanic.com');
-
-  const loadCookies = async (error, cookie) => {
-    if (error) { console.error(error); }
-
-    if (Object.keys(cookie).length) {
-      cookie.url = 'https://gamestarmechanic.com';
-      cookie.path = '/';
-
-      try {
-        await session.defaultSession.cookies.set(cookie);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-
-    if (deepLink) {
-      mainWindow.webContents.send('newTab', { url: deepLink });
-    } else {
-      mainWindow.webContents.send('newTab', { url: 'https://gamestarmechanic.com' });
-    }
-
-    mainWindow.show();
-    registerShortcuts(mainWindow);
-  };
-
-  const saveCookies = async () => {
-    try {
-      const [cookies] = await mainWindow.webContents.session.cookies.get({ name: 'gsm_gamestarmechanic_com_session' });
-      if (cookies) {
-        storage.set('session_cookie', cookies);
-      } else {
-        storage.remove('session_cookie');
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  mainWindow.once('ready-to-show', () => {
-    storage.get('session_cookie', loadCookies);
-
-    session.defaultSession.webRequest.onBeforeRequest((details, callback) => {
-      if (details.resourceType === 'mainFrame' && !isWhiteListed(details.url) && !details.url.startsWith('devtools')) {
-        callback({ cancel: true });
-      } else {
-        callback({});
-      }
-    });
+  
+  app.whenReady().then(() => {
+    const urlArg = process.argv[1];
+    showURL(urlArg);
   });
+}
 
-  mainWindow.webContents.once('did-attach-webview', () => {
-    webContents.getAllWebContents().forEach((webcontents) => {
-      webcontents.on('did-navigate', saveCookies);
-    });
-  });
-
-  app.on('open-url', (event, url) => {
-    event.preventDefault();
-    if (mainWindow.isDestroyed()) {
-      createWindow();
-    } else {
-
-      mainWindow.webContents.send('newTab', { url: url.replace('gsm://', 'https://') });
-    }
-  });
-};
-
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('open-url', (event, url) => {
+  showURL(url);
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  app.quit();
 });
 
 app.on('login', async (event, webContents, request, authInfo, callback) => {
@@ -163,11 +79,4 @@ app.on('login', async (event, webContents, request, authInfo, callback) => {
   });
 
   callback(username, password);
-});
-
-app.on('will-finish-launching', () => {
-  app.on('open-url', (event, url) => {
-    event.preventDefault();
-    deepLink = url.replace('gsm://', 'https://');
-  });
 });
---
+++ src/window.js
@@ -0,0 +1,102 @@
+const { BrowserWindow, session } = require('electron');
+const storage = require('electron-json-storage');
+const { isWhiteListed } = require('./utils/whiteListDomains');
+
+const createWindow = (initialUrl) => {
+  initialUrl = initialUrl || 'https://gamestarmechanic.com';
+
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 675,
+    show: false,
+    title: "Gamestar Mechanic",
+    webPreferences: {
+      plugins: true,
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      webviewTag: true,
+    },
+  });
+
+  mainWindow.loadURL('about:blank');
+
+  const loadCookies = async (error, cookie) => {
+    if (error) { console.error(error); }
+
+    if (Object.keys(cookie).length) {
+      cookie.url = 'https://gamestarmechanic.com';
+      cookie.path = '/';
+
+      try {
+        await session.defaultSession.cookies.set(cookie);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    mainWindow.loadURL(initialUrl);
+
+    mainWindow.show();
+  };
+
+  const saveCookies = async () => {
+    try {
+      const [cookies] = await mainWindow.webContents.session.cookies.get({ name: 'gsm_gamestarmechanic_com_session' });
+      if (cookies) {
+        storage.set('session_cookie', cookies);
+      } else {
+        storage.remove('session_cookie');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  mainWindow.once('ready-to-show', () => {
+    storage.get('session_cookie', loadCookies);
+
+    session.defaultSession.webRequest.onBeforeRequest((details, callback) => {
+      if (details.resourceType === 'mainFrame' && !isWhiteListed(details.url) && !details.url.startsWith('devtools')) {
+        callback({ cancel: true });
+      } else {
+        callback({});
+      }
+    });
+  });
+  
+  mainWindow.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    showURL(url, true);
+  });
+
+  mainWindow.webContents.once('did-attach-webview', () => {
+    webContents.getAllWebContents().forEach((webcontents) => {
+      webcontents.on('did-navigate', saveCookies);
+    });
+  });
+};
+
+const showURL = (url, newWindow = false) => {
+  const allWindows = BrowserWindow.getAllWindows();
+  const lastWindow = allWindows[allWindows.length-1];
+
+  if (url && url.startsWith("gsm://"))
+    url = url.replace('gsm://', 'https://');
+  
+  if (url && !isWhiteListed(url))
+    return;
+
+  if (newWindow || !lastWindow) {
+    createWindow(url);
+  } else {
+    if (url)
+      lastWindow.loadURL(url);
+    if (lastWindow.isMinimized())
+      lastWindow.restore();
+    lastWindow.focus();
+  }
+}
+
+module.exports = { showURL };
+
